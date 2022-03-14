0cd2e00f9f8ca1f98317dfed41816d48f72fcfed
delet-dis <efimov.036@gmail.com>
2022-03-14 11:36:40 +0700

    ➕ added font awesome and router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/global.css'
+import router from './router'
+
+import '@fortawesome/fontawesome-free/js/all'
 
 createApp(App).mount('#app')
