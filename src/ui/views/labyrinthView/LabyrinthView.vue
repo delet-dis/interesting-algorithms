f34b61f7d6349fa7d0e85031e589e13c750cc45c
delet-dis <efimov.036@gmail.com>
2022-03-17 01:23:08 +0700

    🔎 added configurator changes observing

--- src/ui/views/labyrinthView/LabyrinthView.vue
+++ src/ui/views/labyrinthView/LabyrinthView.vue
@@ -8,39 +8,13 @@
             </div>
             <div class="col-lg-6 col-md-12">
                 <Card>
-                    <table class="table table-bordered">
-                        <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <LabyrinthTable :labyrinthSizingProp="this.getLabyrinthSizing"/>
                 </Card>
             </div>
             <div class="col-lg-3 col-md-12">
-                <Card>Test</Card>
+                <Card>
+                    <LabyrinthConfigurator :labyrinthSizingProp="this.getLabyrinthSizing" v-on:setLabyrinthSizing="setLabyrinthSizing"/>
+                </Card>
             </div>
         </div>
     </div>
@@ -51,10 +25,14 @@ import {Options, setup, Vue} from 'vue-class-component';
 import Card from "@/ui/components/card/Card.vue";
 import {useMeta} from "vue-meta";
 import LabyrinthDescription from "@/ui/views/labyrinthView/components/LabyrinthDescription.vue";
+import LabyrinthTable from "@/ui/views/labyrinthView/components/LabyrinthTable.vue";
+import LabyrinthConfigurator from "@/ui/views/labyrinthView/components/LabyrinthConfigurator.vue";
 
 
 @Options({
     components: {
+        LabyrinthConfigurator,
+        LabyrinthTable,
         LabyrinthDescription,
         Card
     },
@@ -64,9 +42,22 @@ export default class LabyrinthView extends Vue {
         title: '🗺',
         htmlAttrs: {lang: 'ru', amp: true}
     }))
+
+    labyrinthSizing = 25
+
+    get getLabyrinthSizing(){
+        return this.labyrinthSizing
+    }
+
+    setLabyrinthSizing(newValue: number) {
+        this.labyrinthSizing = newValue
+        console.log(this.labyrinthSizing)
+    }
 }
 </script>
 
-<style scoped>
-/*@import "../../../../node_modules/bootstrap/scss/_tables.scss";*/
+<style>
+p {
+    font-size: 20px;
+}
 </style>
---
+++ src/ui/views/labyrinthView/components/LabyrinthTable.vue
@@ -0,0 +1,33 @@
+<template>
+    <table class="table">
+        <tbody>
+        <tr v-for="n in getLabyrinthSizingProp" :key="n">
+            <td v-for="n in getLabyrinthSizingProp" :key="n">z</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import {Options, prop, Vue} from 'vue-class-component';
+
+class Props {
+    labyrinthSizingProp: number = prop({
+        required: true,
+    })
+}
+
+@Options({
+    components: {
+    },
+})
+export default class LabyrinthDescription extends Vue.with(Props) {
+    get getLabyrinthSizingProp(){
+        return this.labyrinthSizingProp
+    }
+}
+</script>
+
+<style scoped >
+
+</style>
