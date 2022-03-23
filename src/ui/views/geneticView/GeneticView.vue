35d1f5df06dbe5e8ace7e2c2cf7df9b011d4dbf4
delet-dis <efimov.036@gmail.com>
2022-03-23 18:12:41 +0700

    🧬 added genetic algorithm page

---
+++ src/ui/views/geneticView/GeneticView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-md-12 columnCentered">
+                <Card class="descriptionCard">
+                    <GeneticText/>
+                </Card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, setup, Vue} from 'vue-class-component';
+import Card from "@/ui/components/card/Card.vue";
+import {useMeta} from "vue-meta";
+import "../../../../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
+import GeneticText from "@/ui/views/geneticView/components/GeneticText.vue";
+
+
+@Options({
+    components: {
+        GeneticText,
+        Card
+    },
+})
+export default class GeneticView extends Vue {
+    meta = setup(() => useMeta({
+        title: '🧬',
+        htmlAttrs: { lang: 'ru', amp: true }
+    }))
+}
+</script>
+
+<style scoped>
+
+</style>
