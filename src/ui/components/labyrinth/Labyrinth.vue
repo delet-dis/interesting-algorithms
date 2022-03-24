30e3285bf1e44efde268c2305bdac1182c4d09cf
delet-dis <efimov.036@gmail.com>
2022-03-24 11:10:46 +0700

    ⬆️ extracted labyrinth

---
+++ src/ui/components/labyrinth/Labyrinth.vue
@@ -0,0 +1,101 @@
+<template>
+    <table class="table cardCenterChild">
+        <tbody>
+        <tr v-for="firstIterator in getLabyrinthSizing" :key="firstIterator">
+            <td v-for="secondIterator in getLabyrinthSizing" :key="secondIterator"
+                :id="`table-cell-` + (secondIterator-1) + `x` + (firstIterator-1)" class="table-cell">
+                &nbsp;
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import {Options, prop, Vue} from 'vue-class-component';
+import Card from "@/ui/components/card/Card.vue";
+import SidebarLinkEntity from "@/data/models/SidebarLinkEntity";
+
+class Props {
+    labyrinthSizing: SidebarLinkEntity = prop({
+        required: true,
+    })
+}
+
+@Options({
+    components: {Card},
+})
+export default class Labyrinth extends Vue.with(Props) {
+    private get getLabyrinthSizing() {
+        return this.labyrinthSizing
+    }
+}
+</script>
+
+<style scoped>
+.table {
+    table-layout: fixed;
+    height: 100%;
+
+    border-spacing: 0;
+}
+
+.table-cell {
+    width: auto;
+    height: auto;
+
+    word-break: break-all;
+
+    border: 1px solid black;
+
+    transition: 0.3s;
+}
+
+.table-cell.table-cell-border {
+    border: 1px solid #808080;
+
+    background-color: #545454;
+}
+
+.table-cell.table-cell-start {
+    border: 1px solid #b9e563;
+
+    background-color: #A5DE37;
+}
+
+.table-cell.table-cell-finish {
+    border: 1px solid #FF4351;
+
+    background-color: #FF4351;
+}
+
+.table-cell.table-cell-wrong-path {
+    border: 1px solid #fec04e;
+
+    background-color: #FEAE1B;
+}
+
+.table-cell.table-cell-correct-path {
+    border: 1px solid #a49ef0;
+
+    background-color: #7B72E9;
+}
+
+.table-cell.table-cell-startable:hover {
+    background-color: #A5DE37;
+
+    opacity: 0.3;
+}
+
+.table-cell.table-cell-finishable:hover {
+    background-color: #FF4351;
+
+    opacity: 0.3;
+}
+
+.table-cell.table-cell-borderable:hover {
+    background-color: #545454;
+
+    opacity: 0.3;
+}
+</style>
--- src/ui/views/labyrinthView/LabyrinthView.vue
+++ src/ui/views/labyrinthView/LabyrinthView.vue
@@ -16,16 +16,7 @@
             </div>
             <div class="col-lg-6 col-md-12">
                 <Card class="cardCenter labyrinthCard" id="labyrinthCard">
-                    <table class="table cardCenterChild">
-                        <tbody>
-                        <tr v-for="firstIterator in labyrinthSizing" :key="firstIterator">
-                            <td v-for="secondIterator in labyrinthSizing" :key="secondIterator"
-                                :id="`table-cell-` + (secondIterator-1) + `x` + (firstIterator-1)" class="table-cell">
-                                &nbsp;
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <Labyrinth :labyrinthSizing="labyrinthSizing"/>
                 </Card>
             </div>
             <div class="col-lg-3 col-md-12">
@@ -118,10 +109,12 @@ import LabyrinthSolverRepository from "@/data/repositories/labyrinth/LabyrinthSo
 import LabyrinthSolution from "@/data/models/labyrinth/LabyrinthSolution";
 import LabyrinthDisplayType from "@/ui/views/labyrinthView/enums/LabyrinthDisplayType";
 import Error from "@/ui/components/error/Error.vue";
+import Labyrinth from "@/ui/components/labyrinth/Labyrinth.vue";
 
 
 @Options({
     components: {
+        Labyrinth,
         LabyrinthDescription,
         VueSlider,
         Card,
@@ -502,69 +495,5 @@ export default class LabyrinthView extends Vue {
 </script>
 
 <style scoped>
-.table {
-    table-layout: fixed;
-    height: 100%;
 
-    border-spacing: 0;
-}
-
-.table-cell {
-    width: auto;
-    height: auto;
-
-    word-break: break-all;
-
-    border: 1px solid black;
-
-    transition: 0.3s;
-}
-
-.table-cell.table-cell-border {
-    border: 1px solid #808080;
-
-    background-color: #545454;
-}
-
-.table-cell.table-cell-start {
-    border: 1px solid #b9e563;
-
-    background-color: #A5DE37;
-}
-
-.table-cell.table-cell-finish {
-    border: 1px solid #FF4351;
-
-    background-color: #FF4351;
-}
-
-.table-cell.table-cell-wrong-path {
-    border: 1px solid #fec04e;
-
-    background-color: #FEAE1B;
-}
-
-.table-cell.table-cell-correct-path {
-    border: 1px solid #a49ef0;
-
-    background-color: #7B72E9;
-}
-
-.table-cell.table-cell-startable:hover {
-    background-color: #A5DE37;
-
-    opacity: 0.3;
-}
-
-.table-cell.table-cell-finishable:hover {
-    background-color: #FF4351;
-
-    opacity: 0.3;
-}
-
-.table-cell.table-cell-borderable:hover {
-    background-color: #545454;
-
-    opacity: 0.3;
-}
 </style>
