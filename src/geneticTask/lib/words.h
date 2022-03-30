294c7fd7fe4ecddc596e66d6ff1bff54567df4db
Artem Trushkin <at.120@ya.ru>
2022-03-30 18:52:13 +0700

    new prefix

--- src/geneticTask/lib/words.h
+++ src/geneticTask/lib/words.h
@@ -19,13 +19,14 @@ namespace prefixes{
     const u_int8_t valueMask  = 0b00011111;
     
     enum : u_int8_t {
-        NOTHING         = 0b00000000,
-        EX_VAR          = 0b00100000,
-        CONST           = 0b01000000,
-        FUNC            = 0b01100000,
-        IMMUTABLE       = 0b10000000,
-        OPERATOR        = 0b10100000,
-        COMP_OPERATOR   = 0b11000000,
+        NOTHING             = 0b00000000,
+        FUNC                = 0b00100000,
+        EX_VAR              = 0b01000000,
+        CONST               = 0b01100000,
+        EX_VAR_EXCEPT_LOCAL = 0b10000000,
+        OPERATOR            = 0b10100000,
+        COMP_OPERATOR       = 0b11000000,
+        IMMUTABLE           = 0b11100000,
     };
     
     const u_int8_t minDiff = 0b00100000;
@@ -36,13 +37,13 @@ namespace prefixes{
     };
     
     const prefTemplate templates[7][3] = {
-        {{IMMUTABLE, 0}, {CONST,         0}, {NOTHING, 0}},  //NEW_VAR
-        {{IMMUTABLE, 0}, {IMMUTABLE,     0}, {EX_VAR,  0}},  //DEF
-        {{IMMUTABLE, 0}, {CONST,         1}, {NOTHING, 0}},  //FOR  
-        {{EX_VAR,    0}, {COMP_OPERATOR, 0}, {CONST,   1}},  //IF
-        {{OPERATOR,  0}, {FUNC,          2}, {EX_VAR,  1}},  //EX_VAR third needs to additional check
-        {{EX_VAR,    0}, {NOTHING,       0}, {NOTHING, 0}},  //PRINT 
-        {{EX_VAR,    0}, {NOTHING,       0}, {NOTHING, 0}},  //INPUT
+        {{IMMUTABLE, 0}, {CONST,               0}, {NOTHING,              0}},  //NEW_VAR
+        {{IMMUTABLE, 0}, {IMMUTABLE,           0}, {EX_VAR_EXCEPT_LOCAL,  0}},  //DEF
+        {{IMMUTABLE, 0}, {EX_VAR_EXCEPT_LOCAL, 1}, {NOTHING,              0}},  //FOR  
+        {{EX_VAR,    0}, {COMP_OPERATOR,       0}, {EX_VAR_EXCEPT_LOCAL,  1}},  //IF
+        {{OPERATOR,  0}, {CONST,               2}, {EX_VAR,               0}},  //EX_VAR third needs to additional check
+        {{EX_VAR,    0}, {NOTHING,             0}, {NOTHING,              0}},  //PRINT 
+        {{EX_VAR,    0}, {NOTHING,             0}, {NOTHING,              0}},  //INPUT
     };
     
     int32_t get_template(const u_int8_t word0);
