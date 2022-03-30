5393768130a7d584d2ffcd04c605f223a4c279f0
Artem Trushkin <at.120@ya.ru>
2022-03-30 17:36:21 +0700

    dependencies reworked

--- src/geneticTask/lib/dependencies.cpp
+++ src/geneticTask/lib/dependencies.cpp
@@ -9,33 +9,6 @@ Deps::Deps(const Deps &other) {
     memcpy(this->scopes, other.scopes, sizeof scopes);
 }
 
-void Deps::inc_deps(const u_int8_t ID, dep_type type) {
-    switch (type) {
-        case dep_type::FUNC :
-            funcs[ID]++;
-            break;
-        case dep_type::VAR :
-            vars[ID]++;
-            break;
-        case dep_type::SCOPE :
-            scopes[ID]++;
-            break;
-    }
-}
-
-void Deps::dec_deps(const u_int8_t ID, dep_type type) {
-    switch (type) {
-        case dep_type::FUNC :
-            funcs[ID]--;
-            break;
-        case dep_type::VAR :
-            vars[ID]--;
-            break;
-        case dep_type::SCOPE :
-            scopes[ID]--;
-            break;
-    }
-}
 
 int Deps::get_deps(const Line *l) const {
     if (l->content.word0 > word0::IF)
@@ -62,13 +35,9 @@ int Deps::get_deps(const Line *l) const {
     return deps;
 }
 
-void Deps::free(const Line* l, const u_int8_t parentScope) {
-    scopes[parentScope]--;
-    free(l);
-}
-
-
-void Deps::free(const Line* l) {
+void Deps::free(const Line* l, const u_int8_t scope) {
+    scopes[scope]--;
+    
     for (int i = 1; i < 4; i++) {
         u_int8_t prefix = l->words[i] & prefixes::prefixMask;
         u_int8_t value = l->words[i] & prefixes::valueMask;
--- src/geneticTask/lib/dependencies.h
+++ src/geneticTask/lib/dependencies.h
@@ -1,27 +1,14 @@
 #pragma once
 #include "line.h"
 
-class Line;
-
-enum struct dep_type : u_int8_t {
-    SCOPE,
-    FUNC,
-    VAR,
-};
-
 class Deps {
-private:
+public:
     u_int8_t vars[32];
     u_int8_t funcs[32];
     u_int8_t scopes[32];
-    
-public:
     Deps() = default;
     Deps(const Deps &other);
-    void inc_deps(const u_int8_t ID, dep_type type);
-    void dec_deps(const u_int8_t ID, dep_type type);
     int get_deps(const Line *l) const;
-    void free(const Line *l);
-    void free(const Line *l, const u_int8_t parentScope);
+    void free(const Line *l, const u_int8_t scope);
 };
 
