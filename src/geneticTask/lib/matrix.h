b5cf92673f7b226d2b37a4ee80e18c4b190bd678
Artem Trushkin <at.120@ya.ru>
2022-03-31 13:23:49 +0700

    matrix implementation moved to header

--- src/geneticTask/lib/matrix.h
+++ src/geneticTask/lib/matrix.h
@@ -8,10 +8,22 @@ private:
 public:
     int cols;
     int rows;
-    
-    Matrix(const int columns, const int rows, const bool clear = false);
-    
-    ~Matrix();
-    
-    T* operator[](const int pos);
+
+    Matrix(const int columns, const int rows, const bool clear = false) {
+        this->cols = columns;
+        this->rows = rows;
+        if(clear)
+            this->data = new T[columns * rows]();
+        else
+            this->data = new T[columns * rows];
+    }
+
+    ~Matrix() {
+        delete [] data;
+    }
+
+    T* operator[](const int pos) {
+        return &data[pos * cols];
+    }
+
 };
