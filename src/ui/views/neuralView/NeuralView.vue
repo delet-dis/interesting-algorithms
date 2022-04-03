acc2a3b15916090f31b93bb795527cff9d3cc3b9
delet-dis <efimov.036@gmail.com>
2022-04-03 18:47:21 +0700

    📦 made canvas square

--- src/ui/views/neuralView/NeuralView.vue
+++ src/ui/views/neuralView/NeuralView.vue
@@ -90,6 +90,8 @@ export default class NeuralView extends Vue {
     private static updateCardSize(card: HTMLElement | null) {
         if (card) {
             card.style.height = card.clientWidth + `px`
+
+            card.setAttribute('height', card.clientWidth + `px`)
         }
     }
 
@@ -98,14 +100,16 @@ export default class NeuralView extends Vue {
 
         if (clusteringCard) {
             if (this.canvas) {
-                let clusteringCardOffsetWidth = clusteringCard.offsetWidth - 30
-                let clusteringCardOffsetHeight = clusteringCard.offsetHeight - 40
+                let widthAndHeightDimensions = clusteringCard.clientWidth - 20
 
-                this.canvas.width = clusteringCardOffsetWidth
-                this.canvas.height = clusteringCardOffsetHeight
+                this.canvas.width = widthAndHeightDimensions
+                this.canvas.height = widthAndHeightDimensions
 
-                this.canvas.style.width = clusteringCardOffsetWidth + "px"
-                this.canvas.style.height = clusteringCardOffsetHeight + "px"
+                this.canvas.style.width = widthAndHeightDimensions + "px"
+                this.canvas.style.height = widthAndHeightDimensions + "px"
+
+                this.canvas.setAttribute('width', widthAndHeightDimensions + "px")
+                this.canvas.setAttribute('height', widthAndHeightDimensions + "px")
             }
         }
     }
@@ -296,6 +300,11 @@ export default class NeuralView extends Vue {
 </script>
 
 <style scoped>
+.neuralCard {
+    padding-top: 0.5em;
+    padding-bottom: 0;
+}
+
 .neuralCanvas {
     border-radius: 0.5em;
 
