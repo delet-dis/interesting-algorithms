24f35bf907d622fc77a7d637bb8dcd67320cd2f4
Artem Trushkin <at.120@ya.ru>
2022-04-04 13:09:15 +0700

    edit distance = 18

--- src/geneticTask/lib/gentic_coefficients.h
+++ src/geneticTask/lib/gentic_coefficients.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define SKIP_EDITING_LINE !randint(0, 2) 
-#define SKIP_ADDING_NEW_LINE randint(0, 2)
-#define DELETE_LINE randint(0, 5)
+#define SKIP_EDITING_LINE !randint(0, 1) 
+#define SKIP_ADDING_NEW_LINE !randint(0, 3)
+#define DELETE_LINE randint(0, 3)
 
-const int numOfGenerations = 10;
-const int numOfRandParents = 100;
+const int numOfGenerations = 1000;
+const int numOfRandParents = 5;
 const int numOfEliteParents = 10;
 const int numOfParents = numOfEliteParents + numOfRandParents;
-const int fertility = 3000;
+const int fertility = 100;
 const int numOfChildren = numOfParents * fertility;
