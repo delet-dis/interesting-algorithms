fdb30dafa65b55c7079424eb984a6908fd8df9ca
Artem Trushkin <at.120@ya.ru>
2022-04-04 12:21:21 +0700

    rand() no longer blocks threads

--- src/geneticTask/lib/utils.cpp
+++ src/geneticTask/lib/utils.cpp
@@ -1,5 +1,11 @@
 #include "utils.h"
-#include <cstdlib>
+thread_local int __rand_seed;
+
+unsigned int rand() {
+    __rand_seed = (214013 * __rand_seed + 2531011); 
+    return (__rand_seed >> 16) & 0x7FFF; 
+}
+
 
 int randint(int a, int b) {
     return rand() % (b - a + 1) + a;
