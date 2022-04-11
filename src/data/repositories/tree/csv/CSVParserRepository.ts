d0463f98e07adb351bdcec6c8503575c1fbf37b8
Artem Trushkin <at.120@ya.ru>
2022-04-11 22:46:37 +0700

    parser has been done

--- src/data/repositories/tree/csv/CSVParserRepository.ts
+++ src/data/repositories/tree/csv/CSVParserRepository.ts
@@ -11,8 +11,56 @@ class CSVParserRepository implements CSVParserInterface {
         return CSVParserRepository.instance
     }
 
-    parseInputData(inputData: string): string[][] {
-        return [];
+    private countRows(inputData: string): number {
+        let rows = 1;
+        for (const char of inputData) {
+            if(char == ",")
+                rows++
+            else if(char == "\n")
+                break;
+        }
+        return rows;
+    }
+
+    public parseInputData(inputData: string): string[][] | null {       
+        let column = 0
+        let row = 0
+        let parameterLen = 0
+        const totalRows = this.countRows(inputData);
+        const table: string[][] = []
+        table.push(Array(totalRows).fill(""))
+    
+        for (const char of inputData) {
+            if (char == ",") {
+                if (!parameterLen)
+                    return null
+                row++
+                parameterLen = 0
+                continue
+            }
+    
+            else if (char == "\n") { 
+                if(row != totalRows - 1)
+                    return null
+                table.push(Array(totalRows).fill(""))
+                column++
+                row = 0
+                continue
+            }
+            
+            if(row >= totalRows)
+                return null
+
+            table[column][row] += char
+            parameterLen++
+        }
+
+        if(inputData[inputData.length - 1] == '\n') 
+            table.pop()
+        else if(row != totalRows - 1 || !parameterLen)
+            return null
+
+        return table;
     }
 }
 
