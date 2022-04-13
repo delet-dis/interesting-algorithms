ab6cb5c48bebaaaa217e1d99a3e79182eef3109a
Artem Trushkin <at.120@ya.ru>
2022-04-13 11:18:14 +0700

    exp executor has been done

--- src/data/repositories/tree/TreeExpressionExecutorRepository.ts
+++ src/data/repositories/tree/TreeExpressionExecutorRepository.ts
@@ -1,5 +1,7 @@
 import TreeExpressionExecutorInterface from "@/data/interfaces/tree/TreeExpressionExecutorInterface"
 import Node from "@/data/models/tree/Node"
+import NodeType from "@/data/models/tree/NodeType"
+import CSVParserRepository from "./csv/CSVParserRepository"
 
 class TreeExpressionExecutorRepository implements TreeExpressionExecutorInterface{
     private static instance: TreeExpressionExecutorRepository
@@ -12,8 +14,45 @@ class TreeExpressionExecutorRepository implements TreeExpressionExecutorInterfac
         return TreeExpressionExecutorRepository.instance
     }
 
-    executeExpressionInTree(expression: string, tree: Node[]): Node[] | null{
-        return []
+    private checkCondition(value: string, condition: string): boolean | null {
+
+        if (condition.charCodeAt(0) >= 60 && condition.charCodeAt(0) <= 62) {
+            if (Number(value)) //нет необходимости
+                return eval(value + condition) 
+            else
+                return null
+        }
+
+        return value == condition
+    }
+
+
+    public executeExpressionInTree(expression: string, tree: Node[]): Node[] | null{
+        const parameters = CSVParserRepository.getInstance().parseInputData(expression)
+        
+        if (!parameters)
+            return null
+
+        let currentNode: Node = tree[0]
+        for (const node of tree) {
+            if(this.checkCondition(parameters[node.data.responsibleParameter], node.data.condition)) {
+                currentNode = node
+                break
+            }
+        }
+
+        while (currentNode.data.type != NodeType.LEAF_NODE) {
+            currentNode.data.type = NodeType.PATH_NODE
+            for (const node of currentNode.nestedNodes!) {
+                if(this.checkCondition(parameters[node.data.responsibleParameter], node.data.condition)) {
+                    currentNode = node
+                    break
+                }
+            }
+        }
+        
+        return tree
+        
     }
 }
 
