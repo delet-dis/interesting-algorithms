c299d9ab3d0f5d7b6f36172dc6f004bdd80ed833
delet-dis <efimov.036@gmail.com>
2022-04-15 23:23:11 +0700

    ✏️ changed DividerRepository

---
+++ src/data/repositories/tree/DividerRepository.ts
@@ -0,0 +1,28 @@
+import CSVParserRepository from "./csv/CSVParserRepository"
+import DividerRepositoryBase from "@/data/interfaces/tree/DividerRepositoryBase";
+
+class DividerRepository extends DividerRepositoryBase {
+    private static instance: DividerRepository
+
+    public static getInstance(): DividerRepository {
+        if (!DividerRepository.instance) {
+            DividerRepository.instance = new DividerRepository()
+        }
+
+        return DividerRepository.instance
+    }
+
+    public trainingSample: string[][] | null = null
+    public checkSample: string[][] | null = null
+
+    public divideBySamples(inputData: string) {
+        const sample = CSVParserRepository.getInstance().parseInputData(inputData)
+        if (sample && sample.length > 10) {
+            const dividePoint = Math.floor(sample.length * 0.7)
+            this.trainingSample = sample.slice(0, dividePoint)
+            this.checkSample = sample.slice(dividePoint, sample.length)
+        }
+    }
+}
+
+export default DividerRepository
