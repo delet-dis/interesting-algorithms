c2692f5b08d79620a1a36f74789623a3911441b5
Artem Trushkin <at.120@ya.ru>
2022-04-16 01:07:21 +0700

    tree reducer

--- src/data/repositories/tree/TreeExpressionExecutorRepository.ts
+++ src/data/repositories/tree/TreeExpressionExecutorRepository.ts
@@ -83,21 +83,26 @@ class TreeExpressionExecutorRepository implements TreeExpressionExecutorInterfac
     }
 
 
-    public countLeafResult(expression: string, tree: DisplayingNode): void {
-        const parameters = CSVParserRepository.getInstance().parseInputData(expression)
+    public countLeafResult(parameters: string[], tree: DisplayingNode): boolean {
         
         if (!parameters)
-            return
+            return false
 
         let currentNode = tree
+        let prevNode = new DisplayingNode(this.emptyData, null)
 
         while (currentNode.data.type != NodeType.LEAF_NODE) {
             if (currentNode.nestedNodes) {
+                if(prevNode == currentNode)
+                    return false
+                prevNode = currentNode
                 for (const node of currentNode.nestedNodes) {
                     if (node.data.responsibleParameter && node.data.condition) {
-                        const result = this.checkCondition(parameters[0][node.data.responsibleParameter - 1], node.data.condition)
+                        const result = this.checkCondition(parameters[node.data.responsibleParameter - 1], node.data.condition)
                         if (result) {
+
                             currentNode = node
+                            
                             break
                         }
                     }
@@ -105,6 +110,7 @@ class TreeExpressionExecutorRepository implements TreeExpressionExecutorInterfac
             }
         }
         currentNode.data.counter++
+        return true
     }
 }
 
--- src/data/repositories/tree/TreeReducerRepository.ts
+++ src/data/repositories/tree/TreeReducerRepository.ts
@@ -1,5 +1,10 @@
 import DisplayingNode from "@/data/models/tree/DisplayingNode"
 import TreeReducerInterface from "@/data/interfaces/tree/TreeReducerInterface"
+import DividerRepository from "./DividerRepository"
+import TreeExpressionExecutorRepository from "./TreeExpressionExecutorRepository"
+import NodeType from "@/data/models/tree/NodeType"
+import NodeData from "@/data/models/tree/NodeData"
+
 
 class TreeReducerRepository implements TreeReducerInterface {
     private static instance: TreeReducerRepository
@@ -12,7 +17,68 @@ class TreeReducerRepository implements TreeReducerInterface {
         return TreeReducerRepository.instance
     }
 
+    private emptyNode = new DisplayingNode(new NodeData(0), null)
+
+
+    private tryToRemoveNode(curNode: DisplayingNode): void {
+        
+        
+        if (!curNode.nestedNodes)
+            return
+        
+        let numOfLeafs = 0  
+        for (const node of curNode.nestedNodes)  {
+            this.tryToRemoveNode(node)
+            if(node.data.type == NodeType.LEAF_NODE)
+                numOfLeafs++
+        }
+                
+        
+
+        if(numOfLeafs == curNode.nestedNodes.length) {
+            let maxNode1: DisplayingNode = this.emptyNode
+            let maxNode2: DisplayingNode = this.emptyNode
+            let counter = 0
+            for (const node of curNode.nestedNodes) {
+                if (node.data.counter > maxNode1.data.counter)
+                    maxNode1 = node
+                else if (node.data.counter > maxNode2.data.counter)
+                    maxNode2 = node
+                counter += node.data.counter
+            }
+
+            if (maxNode1.data.counter == 0 || maxNode2.data.counter / maxNode1.data.counter < 1/7) {
+                curNode.data.type = NodeType.LEAF_NODE
+                curNode.data.result = maxNode1.data.result
+                curNode.data.counter = counter
+                curNode.nestedNodes = []
+            }            
+        }
+
+
+    }
+
     reduceTree(tree: DisplayingNode): DisplayingNode | null {
+        const dataSample = DividerRepository.getInstance().checkSample
+        
+        if (!dataSample)
+            return null
+
+        for (const params of dataSample) {
+            const result = TreeExpressionExecutorRepository.getInstance().countLeafResult(params, tree)
+            if (!result) {
+                console.log("NULL RETURNED")
+                return null 
+
+            }
+                
+
+        }
+            
+        
+        this.tryToRemoveNode(tree)
+        
+
         return null
     }
 }
